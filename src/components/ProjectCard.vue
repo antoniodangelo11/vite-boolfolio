8bdd80448af721acf97fb21e00e8d779650b3892
Antonio D'Angelo <reapercsp@gmail.com>
2023-07-14 17:53:00 +0200

    add project card

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,35 @@
+<script>
+    export default {
+        props: {
+            ObjCard: Object,
+        }
+    };
+</script>
+
+<template>
+    <div 
+        v-for="card in this.ObjCard" 
+        :key="card.id" 
+        class="card h-100 card_pro"
+    >
+        <img :src="'http://localhost:8000/storage/' + card.image" class="card-img-top" :alt="id">
+        <div class="card-body">
+            <h5 class="card-title">{{ card.title }}</h5>
+            <p class="card-text">{{ card.description }}</p>
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">Author: {{ card.author }}</li>
+            <li class="list-group-item">Creation Date: {{ card.creation_date }}</li>
+            <li class="list-group-item">Last Update: {{ card.last_update }}</li>
+            <li class="list-group-item">Collaborators: {{ card.collaborators }}</li>
+            <li class="list-group-item">Technologies: {{ card.technologies[0].name }}</li>
+            <li class="list-group-item">Type: {{ card.type_id }}</li>
+        </ul>
+        <div class="card-body">
+            <a :href="card.link_github" class="card-link">Link Github</a>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
--- src/components/ProjectsList.vue
+++ src/components/ProjectsList.vue
@@ -1,11 +1,85 @@
 <script>
-export default {
+    import axios from 'axios';
+    import ProjectCard from './ProjectCard.vue';
     
-}
+    export default {
+        components: {
+            ProjectCard,
+        },
+        
+        data() {
+            return {
+                arrProjects: [],
+                currentPage: 1,
+                nPages: 0,
+            };
+        },
+        
+        methods: {
+            changePage(page) {
+                this.currentPage = page;
+                this.getProjects();
+            },
+            getProjects() {
+                axios
+                .get('http://localhost:8000/api/project', {
+                    params: {
+                        page: this.currentPage,
+                    },
+                })
+                .then(response => {
+                    this.arrProjects = response.data.data;
+                    this.nPages = response.data.last_page;
+                });
+            }
+        },      
+
+        created() {
+            axios
+            .get('http://localhost:8000/api/project', {
+                params: {
+                    page: this.currentPage,
+                }
+            })
+            .then(response=>{
+                this.arrProjects = response.data.data;
+                this.nPages = response.data.last_page;
+            });
+        },
+    };
 </script>
 
 <template>
-    
+    <main>
+        <div class="container">
+            <ProjectCard :ObjCard="this.arrProjects" />
+        </div>
+    </main>
+
+    <div class="container">
+        <nav>
+            <ul class="pagination">
+                <li class="page-item disabled">
+                    <a class="page-link">Previous</a>
+                </li>
+
+                <li
+                    v-for="page in nPages"
+                    :key="page"
+                    class="page-item"
+                    :class="{ active: page == currentPage }"
+                >
+                    <span class="page-link" @click="changePage(page)">
+                        {{ page }}
+                    </span>
+                </li>
+
+                <li class="page-item">
+                    <a class="page-link" href="#">Next</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </template>
 
 <style lang="scss" scoped>
