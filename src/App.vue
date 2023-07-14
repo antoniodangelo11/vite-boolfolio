8c6f96f8a7731f717675499eeebe565a82f7bc1a
Antonio D'Angelo <reapercsp@gmail.com>
2023-07-14 15:42:45 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+  import ProjectsList from './components/ProjectsList.vue';
+  
+  export default {
+    components: {
+      ProjectsList,
+    }
+  }
+</script>
+
+<template>
+  <ProjectsList/>
+</template>
+
+<style lang="scss">
+@import 'bootstrap/scss/bootstrap';
+</style>
